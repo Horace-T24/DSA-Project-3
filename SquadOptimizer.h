8eef69af88b333ad3094cc10893027662976868a
joshm1103 <joshgeo2003@gmail.com>
2025-08-02 18:20:51 -0400

    Add files via upload

---
+++ SquadOptimizer.h
@@ -0,0 +1,14 @@
+#ifndef SQUADOPTIMIZER_H
+#define SQUADOPTIMIZER_H
+#include "Player_Organizer.h"
+#include "Formations.h"
+#include "Requirements.h"
+
+int SquadScore(){
+  int avgRating = combined player ratings / 11
+  int chemistry = combined chemsitry
+  int score = avgRating + chemsitry;
+  return score;
+}
+
+#endif
